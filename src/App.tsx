08dc8dd8ffa54ad2d3387359c6b2c6c484b07164
Karl Cereno <karlcereno@gmail.com>
2022-01-21 02:33:29 -0500

    Add icon links and changed bio layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
-
 import "./App.scss";
 import NavigationBar from "./Components/Navbar/NavigationBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -20,20 +26,34 @@ function App() {
                   src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80"
                   alt="Karl Anthony Cereno"
                 />
-
                 <div className="details text-center">
                   <h2>Karl Anthony Cereno</h2>
                   <p>Front End Web Developer</p>
                 </div>
-                <div>
-                  <h3>Social Media Links</h3>
+                <div className="icon-group">
+                  <FontAwesomeIcon
+                    className="mx-1"
+                    icon={faEnvelope}
+                    size="2x"
+                  />
+                  <FontAwesomeIcon
+                    className="mx-1"
+                    icon={faInstagram}
+                    size="2x"
+                  />
+                  <FontAwesomeIcon className="mx-1" icon={faGithub} size="2x" />
+                  <FontAwesomeIcon
+                    className="mx-1"
+                    icon={faLinkedin}
+                    size="2x"
+                  />
                 </div>
               </Container>
             </Col>
-            <Col className="bio text-white">
+            <Col className="text-white">
               <Container>
                 <div className="bio">
-                  <h2>Bio</h2>
+                  <h2 className="display-4 py-3">Hello There...</h2>
                   <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Debitis tenetur error dolorem quam eum dolorum, non in porro
