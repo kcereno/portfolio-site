f756d6f38240ca338050821e3a53e3fa0fa558fa
Karl Cereno <karlcereno@gmail.com>
2022-01-21 00:03:09 -0500

    Skeleton structure for bio section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
-import './App.css';
+import "./App.scss";
+import NavigationBar from "./Components/Navbar/NavigationBar";
 
 function App() {
   return (
     <div className="App">
-{/* Nav */}
-{/* Bio Section */}
-{/* Skill Section */}
-{/* Projects Section */}
-{/* Contact Section */}
-{/* Footer */}
+      <NavigationBar />
+
+      {/* Home Section */}
+      <section id="home" className="home-section">
+        <Container>
+          <Row className="py-5">
+            <Col className="profile col-lg-4 col-12 text-white">
+              <Container>
+                <div className="avatar">
+                  <h2>Profile Pic</h2>
+                </div>
+                <div className="name">
+                  <h3>Karl Anthony Cereno</h3>
+                  <p>Front End Web Developer</p>
+                </div>
+                <div>
+                  <h3>Social Media Links</h3>
+                </div>
+              </Container>
+            </Col>
+            <Col className="bio text-white">
+              <Container>
+                <div className="bio">
+                <h2>Bio</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis tenetur error dolorem quam eum dolorum, non in porro rerum recusandae vero voluptate rem. Quo explicabo ad distinctio quae. Voluptate, qui.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis tenetur error dolorem quam eum dolorum, non in porro rerum recusandae vero voluptate rem. Quo explicabo ad distinctio quae. Voluptate, qui.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis tenetur error dolorem quam eum dolorum, non in porro rerum recusandae vero voluptate rem. Quo explicabo ad distinctio quae. Voluptate, qui.</p>
+                </div>
+          
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* Skill Section */}
+      {/* Projects Section */}
+      {/* Contact Section */}
+      {/* Footer */}
     </div>
   );
 }
---
+++ src/Components/Navbar/NavigationBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+export default function NavigationBar() {
+  return (
+    <React.Fragment>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Karl Anthony Cereno</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#resume">Resume</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </React.Fragment>
+  );
+}
