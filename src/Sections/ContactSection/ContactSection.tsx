8c355f68fec8e61ab1e199763c36216b2f5472d5
Karl Cereno <karlcereno@gmail.com>
2022-01-25 23:59:14 -0500

    Install emailjs and hook it up to contact section

--- src/Sections/ContactSection/ContactSection.tsx
+++ src/Sections/ContactSection/ContactSection.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react";
 import { Container, Col, Form, Button, Row } from "react-bootstrap";
+import emailjs from "@emailjs/browser";
 
 export default function ContactSection() {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  const emailForm = () => {
+    emailjs
+      .send(
+        "service_2qqh8n9",
+        "template_t78185m",
+        toSend,
+        "user_rNMPCxOjM6uqr58jypKm6"
+      )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err.text);
+      });
+  };
+
+  const resetForm = () => {
+    setToSend({
+      from_name: "",
+      message: "",
+      reply_to: "",
+    });
+  };
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+
+    emailForm();
+    alert("message sent");
+    resetForm();
+  };
+
+  const handleChange = (e: any) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <section id="contact" className="text-white py-5">
       <Container>
@@ -10,17 +54,42 @@ export default function ContactSection() {
           </Col>
           <Col>
             <div>
-              <Form>
+              <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="contactName">
-                  <Form.Control type="text" placeholder="Name" />
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Name"
+                    name="from_name"
+                    onChange={handleChange}
+                    value={toSend.from_name}
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="contactEmail">
-                  <Form.Control type="email" placeholder="Email" />
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    name="reply_to"
+                    onChange={handleChange}
+                    value={toSend.reply_to}
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="contactMessage">
-                  <Form.Control as="textarea" placeholder="Message" rows={3} />
+                  <Form.Control
+                    required
+                    as="textarea"
+                    placeholder="Message"
+                    name="message"
+                    rows={3}
+                    onChange={handleChange}
+                    value={toSend.message}
+                  />
                 </Form.Group>
-                <Button className="button" variant="outline-light">
+                <Button
+                  className="button"
+                  type="submit"
+                  variant="outline-light"
+                >
                   Send
                 </Button>
               </Form>
@@ -31,3 +100,5 @@ export default function ContactSection() {
     </section>
   );
 }
+
+//service_2qqh8n9
