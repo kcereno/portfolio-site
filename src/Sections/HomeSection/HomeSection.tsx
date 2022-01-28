35b71c372bc6e94cec77d0f10b2904b4b92f125d
Karl Cereno <karlcereno@gmail.com>
2022-01-27 23:16:29 -0500

    Remove NavBrand and center NavLinks

--- src/Sections/HomeSection/HomeSection.tsx
+++ src/Sections/HomeSection/HomeSection.tsx
@@ -76,7 +76,7 @@ export default function HomeSection() {
                   My name is Karl and I am a web developer based in NYC and I
                   make <span style={{ color: "#ff2957" }}>modern</span> and
                   <span style={{ color: "#ff2957" }}> responsive</span> websites written
-                  with elegant, easy to read and maintanable code.
+                  with elegant, easy to read and maintainable code.
                 </h3>
               </div>
             </Container>
--- src/Sections/NavigationBar/NavigationBar.tsx
+++ src/Sections/NavigationBar/NavigationBar.tsx
@@ -7,15 +7,13 @@ export default function NavigationBar() {
     <React.Fragment>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home" className={styles.logo}>
-            <span className={styles.logo}>Karl Cereno</span>
-          </Navbar.Brand>
+  
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
             id="responsive-navbar-nav"
             className="justify-content-end"
           >
-            <Nav>
+            <Nav className="m-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#skills">Skills</Nav.Link>
               <Nav.Link href="#projects">Projects</Nav.Link>
