18beed14bbcd505b218930973994cb45497fda5e
Karl Cereno <karlcereno@gmail.com>
2022-01-27 23:07:11 -0500

    Add bio

--- src/Sections/HomeSection/HomeSection.tsx
+++ src/Sections/HomeSection/HomeSection.tsx
@@ -1,96 +1,88 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import profilePicture from '../../assets/pictures/profile1.png'
-import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import profilePicture from "../../assets/pictures/profile1.png";
+import {
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import styles from "./HomeSection.module.css";
 
 export default function HomeSection() {
-
-  return(
+  return (
     <section id="home" className="py-5 text-white">
-    <Container>
-      <Row>
-        <Col md={4} className="profile  ">
-          <Container className="d-flex flex-column align-items-center">
-            <img
-              className="profile-img  py-3"
-              src={profilePicture}
-              alt="Karl Anthony Cereno"
-            />
-            <div className="details text-center">
-              <h2>Karl Anthony Cereno</h2>
-              <p>Front End Web Developer</p>
-            </div>
-            <div className="icon-group">
-              <a
-                href="mailto:karlcereno@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon
-                  className="mx-1 social-icon"
-                  icon={faEnvelope}
-                  size="2x"
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/_karlosPeligroso/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon
-                  className="mx-1"
-                  icon={faInstagram}
-                  size="2x"
-                />
-              </a>
-              <a
-                href="https://github.com/kcereno"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon
-                  className="mx-1"
-                  icon={faGithub}
-                  size="2x"
-                />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/karlanthonycereno/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon
-                  className="mx-1"
-                  icon={faLinkedin}
-                  size="2x"
-                />
-              </a>
-            </div>
-          </Container>
-        </Col>
-        <Col className="d-flex align-items-center">
-          <Container>
-            <div>
-              <h2 className="display-4 py-3">Hello There...</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Debitis tenetur error dolorem quam eum dolorum, non in porro
-                rerum recusandae vero voluptate rem. Quo explicabo ad
-                distinctio quae. Voluptate, qui.Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Debitis tenetur error dolorem
-                quam eum dolorum, non in porro rerum recusandae vero
-                voluptate rem. Quo explicabo ad distinctio quae. Voluptate,
-                qui.Lorem ipsum dolor sit, amet consectetur adipisicing
-                elit. Debitis tenetur error dolorem quam eum dolorum, non in
-                porro rerum recusandae vero voluptate rem. Quo explicabo ad
-                distinctio quae. Voluptate, qui.
-              </p>
-            </div>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-  )
+      <Container>
+        <Row>
+          <Col md={4} className="profile  ">
+            <Container className="d-flex flex-column align-items-center">
+              <img
+                className="profile-img  py-3"
+                src={profilePicture}
+                alt="Karl Anthony Cereno"
+              />
+              <div className="details text-center mb-2">
+                <h2 className={styles.main}>Karl Anthony Cereno</h2>
+              </div>
+              <div className="icon-group">
+                <a
+                  href="mailto:karlcereno@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="mx-1 social-icon"
+                    icon={faEnvelope}
+                    size="2x"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/_karlosPeligroso/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="mx-1"
+                    icon={faInstagram}
+                    size="2x"
+                  />
+                </a>
+                <a
+                  href="https://github.com/kcereno"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon className="mx-1" icon={faGithub} size="2x" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/karlanthonycereno/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="mx-1"
+                    icon={faLinkedin}
+                    size="2x"
+                  />
+                </a>
+              </div>
+            </Container>
+          </Col>
+          <Col className="d-flex align-items-center">
+            <Container>
+              <div>
+                <h2 className="display-4 py-3">Nice To Meet You...</h2>
+                <h3>
+                  My name is Karl and I am a web developer based in NYC and I
+                  make <span style={{ color: "#ff2957" }}>modern</span> and
+                  <span style={{ color: "#ff2957" }}>responsive</span> websites written
+                  with elegant, easy to read and maintanable code.
+                </h3>
+              </div>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
--- src/Sections/NavigationBar/NavigationBar.tsx
+++ src/Sections/NavigationBar/NavigationBar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import styles from "./NavigationBar.module.scss";
 
 export default function NavigationBar() {
   return (
     <React.Fragment>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">KAGC</Navbar.Brand>
+          <Navbar.Brand href="#home" className={styles.logo}>
+            <span className={styles.logo}>Karl Cereno</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
             id="responsive-navbar-nav"
@@ -17,7 +20,6 @@ export default function NavigationBar() {
               <Nav.Link href="#skills">Skills</Nav.Link>
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
-           
             </Nav>
           </Navbar.Collapse>
         </Container>
