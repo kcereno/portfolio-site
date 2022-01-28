28d1eca29bfac44baf83f17c7fa7a3e77ae24318
Karl Cereno <karlcereno@gmail.com>
2022-01-27 22:16:17 -0500

    Fix favicon not showing correctly

--- src/Sections/NavigationBar/NavigationBar.tsx
+++ src/Sections/NavigationBar/NavigationBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
     <React.Fragment>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Karl Anthony Cereno</Navbar.Brand>
+          <Navbar.Brand href="#home">KAGC</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
             id="responsive-navbar-nav"
