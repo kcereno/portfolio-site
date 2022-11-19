e0131913e5cdd7746939f83b37378448e567ca47
Karl Cereno <kagc.dev@gmail.com>
2022-11-19 12:08:41 -0500

    Make rough page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import './App.scss';
 
 function App() {
-  return <div className="App">Hellow World</div>;
+  return (
+    <div className="App">
+      <div className="nav mb-xl">Nav</div>
+
+      <div className="about mb-lg">
+        <div className="profile-pic">profile picture</div>
+        <div className="name">Karl Cereno</div>
+        <div className="subheading">A quick description of what i do</div>
+        <div className="social">Social Links</div>
+      </div>
+      <div className="content">
+        <div className="about">
+          Grid Cell 1. This is where I&apos;ll talk about myself. Take from
+          RAMs. Link to contact form site
+        </div>
+        <div className="skills mb-lg">
+          Grid 2. This is where I&apos;ll talk about my skills
+        </div>
+      </div>
+
+      <div className="projects">Project Section. Take from RAM</div>
+      <div className="contact">Contact Form.</div>
+      <div className="footer">Simple footer. Just name and date</div>
+    </div>
+  );
 }
 
 export default App;
