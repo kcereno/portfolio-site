90d313544b1c80d6b06cb51eea937c8d9fcee0a6
Karl Cereno <kagc.dev@gmail.com>
2022-11-21 11:23:51 -0500

    Style nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,30 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="nav mb-xl">Nav</div>
+      {/* Navigation */}
+      <div className="nav pt-sm mb-xl">
+        <div />
+        <div className="nav-links ">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+          <a className="nav-link" href="/">
+            About
+          </a>
+          <a className="nav-link" href="/">
+            Projects
+          </a>
+          <a className="nav-link" href="/">
+            Contact
+          </a>
+        </div>
+        <div className="pr-sm">Light/Dark Button</div>
+      </div>
 
       <div className="about mb-lg">
-        <div className="profile-pic">profile picture</div>
+        <div className="profile-pic">
+          <p>proflepic</p>
+        </div>
         <div className="name">Karl Cereno</div>
         <div className="subheading">A quick description of what i do</div>
         <div className="social">Social Links</div>
