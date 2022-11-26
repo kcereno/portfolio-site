771457bc48be159b75c2a2c5ee23e5af8776de5a
Karl Cereno <kagc.dev@gmail.com>
2022-11-26 12:59:18 -0500

    Change nav to grid

--- src/sections/Navigation/Navigation.tsx
+++ src/sections/Navigation/Navigation.tsx
@@ -8,28 +8,38 @@ interface Props {
 
 function Navigation({ darkMode, toggleDarkMode }: Props) {
   return (
-    <div className="nav pt-sm mb-xl ">
-      <div>{/* Used to help align other elems in flexbox */}</div>
-      <div className="nav-links ">
-        <a className="nav-link" href="/">
-          Home
-        </a>
-        <a className="nav-link" href="/">
-          About
-        </a>
-        <a className="nav-link" href="/">
-          Projects
-        </a>
-        <a className="nav-link" href="/">
-          Contact
-        </a>
+    <div className="nav pt-sm mb-md ">
+      <div>{/* Used to help alignment purposes */}</div>
+      <div className="nav-links-cell">
+        <div className="nav-links ">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+          <a className="nav-link" href="/">
+            About
+          </a>
+          <a className="nav-link" href="/">
+            Projects
+          </a>
+          <a className="nav-link" href="/">
+            Contact
+          </a>
+        </div>
       </div>
-      <div className="theme-icon-container mx-md">
-        {darkMode ? (
-          <MdOutlineModeNight className="theme-icon" onClick={toggleDarkMode} />
-        ) : (
-          <MdOutlineLightMode className="theme-icon" onClick={toggleDarkMode} />
-        )}
+      <div className="theme-icon-cell">
+        <div className="theme-icon-container mx-md">
+          {darkMode ? (
+            <MdOutlineModeNight
+              className="theme-icon"
+              onClick={toggleDarkMode}
+            />
+          ) : (
+            <MdOutlineLightMode
+              className="theme-icon"
+              onClick={toggleDarkMode}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
