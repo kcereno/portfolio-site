d1ff6236955a5520d223ead8acc513a9270ea7be
Karl Cereno <kagc.dev@gmail.com>
2022-11-30 12:37:13 -0500

    Add hover effect to social links and tweaked hero text

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -13,13 +13,15 @@ function Hero() {
       <h1 className="hero-name mb-xxs">Karl Cereno</h1>
       <h2 className="hero-job mb-sm">Frontend Web Developer</h2>
 
-      <p className="mb-md hero-text">I make pretty things on the internet</p>
+      <p className="mb-md hero-text">
+        I make pretty things on the internet among other things
+      </p>
 
       <div className="social-icons pb-sm">
-        <GrLinkedinOption className="social-icon" />
-        <GrGithub className="social-icon" />
-        <GrInstagram className="social-icon" />
-        <GrMail className="social-icon" />
+        <GrLinkedinOption className="social-icon hvr-grow" />
+        <GrGithub className="social-icon hvr-grow" />
+        <GrInstagram className="social-icon hvr-grow" />
+        <GrMail className="social-icon hvr-grow" />
       </div>
     </section>
   );
