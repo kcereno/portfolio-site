4995503e5d9a2616415e4ee9c8ba2f1a41453062
Karl Cereno <kagc.dev@gmail.com>
2022-12-02 11:47:00 -0500

    Tweak spacing and text on hero

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -8,14 +8,12 @@ import {
 
 function Hero() {
   return (
-    <section id="hero" className="mb-md container fade-in-fwd">
-      <h3 className="hero-intro  mb-sm">Hello, My name is</h3>
-      <h1 className="hero-name mb-xxs">Karl Cereno</h1>
-      <h2 className="hero-job mb-sm">Frontend Web Developer</h2>
+    <section id="hero" className="mb-sm container fade-in-fwd">
+      <h3 className="hero-intro  mb-xs">Hello, My name is</h3>
+      <h1 className="hero-name ">Karl Cereno</h1>
+      <h2 className="hero-job mb-xs">Frontend Web Developer</h2>
 
-      <p className="mb-md hero-text">
-        I make pretty things on the internet and other things
-      </p>
+      <p className="mb-sm hero-text">I make pretty things on the internet</p>
 
       <div className="social-icons pb-sm">
         <GrLinkedinOption className="social-icon hvr-grow" />
