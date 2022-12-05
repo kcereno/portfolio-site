54e401dd573eef583069f6ea4c83747d875f6373
Karl Cereno <kagc.dev@gmail.com>
2022-12-05 12:45:03 -0500

    Change line height on about me text

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -5,17 +5,20 @@ function About() {
     <section id="about" className="about">
       <div className="about-container ">
         <h2 className="mb-xs section-title">A little about me</h2>
-        <p className="mb-xs">
-          I am Frontend Developer from NYC. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua.
-        </p>
-        <p className="mb-xs">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+        <div className="about-text-box">
+          <p className="mb-xs">
+            I am Frontend Developer from NYC. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </p>
+          <p className="mb-xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
         <div>
           <button className="button-8" type="button">
             <span className="text">Contact</span>
