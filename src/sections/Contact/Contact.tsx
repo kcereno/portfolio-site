8c608ce7ce66c9b2872f7f21451ad86ac33c1d22
Karl Cereno <kagc.dev@gmail.com>
2022-12-06 11:23:38 -0500

    Change box size

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -3,26 +3,28 @@ import './Contact.scss';
 function Contact() {
   return (
     <section id="contact" className="my-xl">
-      <div className="contact-container ">
-        <div className="contact-content m-md">
-          <h1 className="mb-md text-center section-title">Reach Out!</h1>
-          <form className="contact-form">
-            <label htmlFor="fullName">
-              Full Name
-              <input type="text" />
-            </label>
-            <label htmlFor="email">
-              Email
-              <input type="email" />
-            </label>
-            <label htmlFor="message">
-              Message
-              <textarea rows={10} cols={10} />
-            </label>
-            <button type="submit" className="button-8">
-              Submit
-            </button>
-          </form>
+      <div className="flex-center-x">
+        <div className="contact-container ">
+          <div className="contact-content m-md ">
+            <h1 className="mb-md text-center section-title">Reach Out!</h1>
+            <form className="contact-form">
+              <label htmlFor="fullName">
+                Full Name
+                <input type="text" />
+              </label>
+              <label htmlFor="email">
+                Email
+                <input type="email" />
+              </label>
+              <label htmlFor="message">
+                Message
+                <textarea rows={10} cols={10} />
+              </label>
+              <button type="submit" className="button-8">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
