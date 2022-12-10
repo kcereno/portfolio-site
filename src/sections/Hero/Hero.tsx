2140ffbbb901b97a33d46b63abb4c964987fb803
Karl Cereno <kagc.dev@gmail.com>
2022-12-10 12:13:15 -0500

    Refactor Hero scss

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -10,13 +10,15 @@ function Hero() {
   return (
     <section id="hero">
       <div className="mt-xl mb-md fade-in-fwd">
-        <h3 className="hero-intro">Hello, My name is</h3>
-        <h1 className="hero-name">Karl Cereno</h1>
-        <h2 className="hero-job mb-sm">Frontend Web Developer</h2>
+        <h3 className="hero__intro">Hello, My name is</h3>
+        <h1 className="hero__name">Karl Cereno</h1>
+        <h2 className="hero__job mb-sm">Frontend Web Developer</h2>
 
-        <p className="mb-md hero-text">I make pretty things on the internet</p>
+        <p className="hero__subheading mb-md ">
+          I make pretty things on the internet
+        </p>
 
-        <div className="social-icons pb-sm">
+        <div className="flex pb-sm">
           <GrLinkedinOption className="social-icon hvr-grow" />
           <GrGithub className="social-icon hvr-grow" />
           <GrInstagram className="social-icon hvr-grow" />
