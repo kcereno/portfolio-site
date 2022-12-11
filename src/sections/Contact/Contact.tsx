f547eb17bf497e456629849048c82c67fdd8c538
Karl Cereno <kagc.dev@gmail.com>
2022-12-11 13:04:40 -0500

    Refactort Contact

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -4,27 +4,25 @@ function Contact() {
   return (
     <section id="contact" className="my-xl">
       <div className="flex-center-x">
-        <div className="contact-container ">
-          <div className="contact-content m-md ">
+        <div className="contact__container">
+          <form className="contact__form m-md">
             <h1 className="mb-md text-center section-title">Reach Out!</h1>
-            <form className="contact-form">
-              <label htmlFor="fullName">
-                Full Name
-                <input type="text" />
-              </label>
-              <label htmlFor="email">
-                Email
-                <input type="email" />
-              </label>
-              <label htmlFor="message">
-                Message
-                <textarea rows={10} cols={10} />
-              </label>
-              <button type="submit" className="button-8">
-                Submit
-              </button>
-            </form>
-          </div>
+            <label htmlFor="fullName">
+              Full Name
+              <input type="text" className="mb-sm" />
+            </label>
+            <label htmlFor="email">
+              Email
+              <input type="email" className="mb-sm" />
+            </label>
+            <label htmlFor="message">
+              Message
+              <textarea rows={10} cols={10} className="mb-sm" />
+            </label>
+            <button type="submit" className="button-8">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </section>
