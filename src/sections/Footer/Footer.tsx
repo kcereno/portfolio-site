c5a3f9a03d5e3d801d9ae4a988762c4d03a81945
Karl Cereno <kagc.dev@gmail.com>
2022-12-12 11:46:50 -0500

    Recfactor Footer CSS

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <div className="">
-      <hr className="footer-divider" />
+      <hr className="footer__divider" />
       <p className="text-center my-lg">{`${currentYear} Karl Cereno`}</p>
     </div>
   );
