7497366c0ae1e4d15b56d659c6b6bae0ab4764d9
Karl Cereno <kagc.dev@gmail.com>
2022-12-19 12:51:54 -0500

    Redesign hero

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -3,32 +3,39 @@ import {
   GrLinkedinOption,
   GrGithub,
   GrInstagram,
-  GrMail,
+  // GrMail,
 } from 'react-icons/gr';
 
 function Hero() {
   return (
     <section id="hero">
-      <div className="mb-xl pt-xl fade-in-fwd  ">
-        <h3 className="text-pink text-lg font-black mb-xs tracking-widest uppercase tablet:text-md">
-          Hello, My name is
-        </h3>
-        <h1 className=" text-4xl leading-display font-display font-semibold tracking-tight tablet:text-3xl tablet:leading-display--tablet">
-          Karl Cereno
-        </h1>
-        <h2 className=" text-4xl  leading-display font-display font-semibold mb-sm tracking-tighter  text-gray-400 tablet:text-3xl tablet:leading-display--tablet">
-          Frontend Web Developer
-        </h2>
-
-        <p className="max-w-prose mb-md text-xl text-gray-500 tablet:text-lg">
-          I make pretty things on the internet
-        </p>
-
-        <div className="flex pb-sm">
-          <GrLinkedinOption className="h-[2.2rem] w-[2.2rem] hover:text-pink mr-md hvr-grow" />
-          <GrGithub className="h-[2.2rem] w-[2.2rem] hover:text-pink mr-md hvr-grow" />
-          <GrInstagram className="h-[2.2rem] w-[2.2rem] hover:text-pink mr-md hvr-grow" />
-          <GrMail className="h-[2.2rem] w-[2.2rem] hover:text-pink mr-md hvr-grow" />
+      <div className="hero__container mb-xl pt-xl fade-in-fwd  ">
+        <div className="hero__text-box">
+          <h3 className="text-pink text-lg font-black mb-xs tracking-widest uppercase tablet:text-md">
+            Hello, My name is
+          </h3>
+          <h1 className=" text-4xl leading-display font-display font-semibold tracking-tight tablet:text-3xl tablet:leading-display--tablet">
+            Karl Cereno
+          </h1>
+          <h2 className=" text-4xl  leading-display font-display font-semibold mb-sm tracking-tighter  text-gray-400 tablet:text-3xl tablet:leading-display--tablet">
+            Frontend Web Developer
+          </h2>
+          <p className="max-w-prose mb-md text-lg text-gray-500 tablet:text-lg">
+            This is where I talk a little about my self. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="hero__contact-links flex gap-sm items-center">
+          <button className="css-button-sliding-to-left--rose" type="button">
+            <span className="text">Say Hello!</span>
+          </button>
+          <div className="">
+            <GrLinkedinOption className="h-[3.2rem] w-[3.2rem] hover:text-pink hvr-grow mx-xs" />
+            <GrGithub className="h-[3.2rem] w-[3.2rem] hover:text-pink hvr-grow mx-xs" />
+            <GrInstagram className="h-[3.2rem] w-[3.2rem] hover:text-pink hvr-grow mx-xs" />
+            {/* <GrMail className="h-[2.2rem] w-[2.2rem] hover:text-pink mr-md hvr-grow" /> */}
+          </div>
         </div>
       </div>
     </section>
